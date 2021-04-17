7de133a1c745ba727501f78489faf9e1c9d9457e
majewskidominik01 <62767834+majewskidominik01@users.noreply.github.com>
2021-04-17 15:49:11 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,30 @@
+var xoro = 1;
+function count1() {
+    xoro++;
+    if (xoro%2) {
+        document.getElementById("1").innerHTML = "X";
+    }
+        else {
+            document.getElementById("1").innerHTML = "O";
+    }    
+}
+
+function count2() {
+    xoro++;
+    if (xoro%2) {
+        document.getElementById("2").innerHTML = "X";
+    }
+        else {
+            document.getElementById("2").innerHTML = "O";
+    }    
+}
+
+function count3() {
+    xoro++;
+    if (xoro%2) {
+        document.getElementById("3").innerHTML = "X";
+    }
+        else {
+            document.getElementById("3").innerHTML = "O";
+    }     
+}

c2f71d6521963b56ffac4a3c224a77bea8a6fd28
Dominik Majewski <majewskidominik01@gmail.com>
2021-04-19 20:20:46 +0200

    Update js

--- js1.js
+++ js1.js
@@ -14,16 +14,12 @@ for (i=0; i<=8; i++) {
     liczbapol[i].addEventListener("click", xoro);
 }
 
+
 function xoro() {
     if(event.target.innerHTML == "X" || event.target.innerHTML == "O") {
-
     } else if(liczbajeden%2) {
-        event.target.innerHTML = "X";
-        liczbajeden++;
         xwin();
     } else {
-        event.target.innerHTML = "O";
-        liczbajeden++;
         owin();
     }
 }
@@ -31,12 +27,17 @@ function xoro() {
 function xwin() {
     if (liczbapol[0].innerHTML == "X" && liczbapol[1].innerHTML == "X" && liczbapol[2].innerHTML == "X" || liczbapol[3].innerHTML == "X" && liczbapol[4].innerHTML == "X" && liczbapol[5].innerHTML == "X" || liczbapol[6].innerHTML == "X" && liczbapol[7].innerHTML == "X" && liczbapol[8].innerHTML == "X" || liczbapol[0].innerHTML == "X" && liczbapol[4].innerHTML == "X" && liczbapol[8].innerHTML == "X" || liczbapol[2].innerHTML == "X" && liczbapol[4].innerHTML == "X" && liczbapol[6].innerHTML == "X" ) {
         console.log("WYGRAŁ X");
-        
+    } else {
+        event.target.innerHTML = "X";
+        liczbajeden++;
     }
 }
 
 function owin() {
     if (liczbapol[0].innerHTML == "O" && liczbapol[1].innerHTML == "O" && liczbapol[2].innerHTML == "O" || liczbapol[3].innerHTML == "O" && liczbapol[4].innerHTML == "O" && liczbapol[5].innerHTML == "O" || liczbapol[6].innerHTML == "O" && liczbapol[7].innerHTML == "O" && liczbapol[8].innerHTML == "O" || liczbapol[0].innerHTML == "O" && liczbapol[4].innerHTML == "O" && liczbapol[8].innerHTML == "O" || liczbapol[2].innerHTML == "O" && liczbapol[4].innerHTML == "O" && liczbapol[6].innerHTML == "O" ) {
         console.log("WYGRAŁ O");
+    } else {
+        event.target.innerHTML = "O";
+        liczbajeden++;
     }
 }
